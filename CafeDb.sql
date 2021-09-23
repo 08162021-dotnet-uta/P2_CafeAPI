2d2b6887ef36bfb09f22baa90c4d63ba46542279
KyleHillRevature <kyle.hill@revature.net>
2021-09-23 01:40:45 -0400

    Made some tweaks to Gulomjon's script for creating MVP tables

--- CafeDb.sql
+++ CafeDb.sql
@@ -1,36 +1,41 @@
 --create Customers table
-CREATE TABLE Customers(
-CustomerId INT PRIMARY KEY IDENTITY,
+CREATE TABLE Customer(
+Id INT PRIMARY KEY IDENTITY,
 FirstName varchar(50) NOT NULL,
 LastName varchar(50) NOT NULL);
 
 -- create Administrator table
 CREATE TABLE Administrator(
-AdministratorId INT PRIMARY KEY IDENTITY,
+Id INT PRIMARY KEY IDENTITY,
 FirstName varchar(50) NOT NULL,
 LastName varchar(50) NOT NULL,
 EmailAddress varchar(50) NOT NULL,
-Role varchar(50));
+[Role] varchar(50));
 
 -- create SingIn table
 CREATE TABLE SignIn(
 UserName varchar(30) NOT NULL,
-Password varchar(30) NOT NULL,
-CustomerId INT NOT NULL FOREIGN KEY REFERENCES Customers(CustomerId) ON DELETE CASCADE,
-AdministratorId INT NOT NULL FOREIGN KEY REFERENCES Administrator(AdministratorId) ON DELETE CASCADE)
+[Password] varchar(30) NOT NULL,
+CustomerId INT FOREIGN KEY REFERENCES Customer(Id) ON DELETE CASCADE,
+AdministratorId INT FOREIGN KEY REFERENCES Administrator(Id) ON DELETE CASCADE)
 
 -- create Products table
-CREATE TABLE Products(
-ProductId INT PRIMARY KEY IDENTITY,
-ProductName varchar(50) NOT NULL,
-ProductDescription varchar(100) NOT NULL,
+CREATE TABLE Product(
+Id INT PRIMARY KEY IDENTITY,
+[Name] varchar(50) NOT NULL,
+[Description] varchar(100),
 Price decimal(20,4) NOT NULL,
-Inventory INT NOT NULL);
+Inventory INT default 10);
 
 -- create Order table
 CREATE TABLE [Order](
-OrderId INT PRIMARY KEY IDENTITY,
-CustomerId INT NOT NULL FOREIGN KEY REFERENCES Customers(CustomerId) ON DELETE CASCADE,
-ProductId INT NOT NULL FOREIGN KEY REFERENCES Products(ProductId) ON DELETE CASCADE,
+Id INT PRIMARY KEY IDENTITY,
+CustomerId INT NOT NULL FOREIGN KEY REFERENCES Customer(Id) ON DELETE CASCADE,
 NumberOfItems INT NOT NULL,
 TotalPrice decimal(20,4) NOT NULL);
+
+-- create OrderItem table
+create table [OrderItem](
+OrderId INT NOT NULL FOREIGN KEY REFERENCES [Order](Id) ON DELETE CASCADE,
+ProductId INT NOT NULL FOREIGN KEY REFERENCES Product(Id) ON DELETE CASCADE
+)
