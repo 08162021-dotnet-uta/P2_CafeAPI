a421218355d13c557316f0c1d9c723a7ac7020f3
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-28 14:58:58 -0600

    added httpclient and routing

--- CafeAPI-Angular/src/app/app-routing.module.ts
+++ CafeAPI-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListingComponent } from './Components/product-listing/product-listing.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: `/`, pathMatch: 'full' },
+  { path: 'productlist', component: ProductListingComponent },
 ];
 
 @NgModule({
