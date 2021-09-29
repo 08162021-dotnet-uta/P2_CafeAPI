e9d3410c928eb715c9c73f095e154f3901227347
Christie Thammavong <cthammavong@mavs.coloradomesa.edu>
2021-09-29 13:57:59 -0600

    changed outOfStock() value to return false for testing purposes

--- CafeAPI-Angular/src/app/Services/product.service.ts
+++ CafeAPI-Angular/src/app/Services/product.service.ts
@@ -15,7 +15,7 @@ export class ProductService {
   //The literal below will replaced by the result of the call to CafeAPI to see is the item is out-of-stock
   //In This is the other input for testing my add-to-cart FE functionality (the other being the "Add to cart" button in app.component)
   //Possible input values for testing: true,false
-  outOfStock(): Observable<boolean>{return of(true)}
+  outOfStock(): Observable<boolean>{return of(false)}
 
   // correctly format the search term to work in API request: 'memory+cards' instead of 'memory cards'
   formatSearchTerm(searchTerm: string): string {
