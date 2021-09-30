cf895e5e8f3e988aa3f96cf46f0028987140b026
Gulomjon Saidov <gulomjon.saidov@revature.net>
2021-09-30 13:02:45 -0700

    added back end for Product with product/list route

---
+++ CafeAPI/StorageLayer/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageLayer.Interfaces
+{
+    public interface IProductRepository
+    {
+        List<ViewModelProduct> Products() { throw new NotImplementedException(); }
+    }
+}
---
+++ CafeAPI/StorageLayer/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using DatabaseLayer;
+using Microsoft.EntityFrameworkCore;
+using ModelsLayer.EfModels;
+using StorageLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageLayer
+{
+    public class ProductRepository : IProductRepository
+    {
+        // Step 1 of Dependency Injection - create a  private instance of the dependency
+        private readonly testprojectContext _context;
+        // Step 2 of DI - call for an instance from the DI system in the constructor.
+        public ProductRepository(testprojectContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<ViewModelProduct> Products()
+        {
+            List<Product> products = _context.Products.FromSqlRaw<Product>("select * from Product").ToList();
+
+            List<ViewModelProduct> vmp = new List<ViewModelProduct>();
+            foreach (Product p in products)
+            {
+                vmp.Add(ModelMapper.ProductToViewModelProduct(p));
+            }
+
+            return vmp;
+        }
+    }
+}
