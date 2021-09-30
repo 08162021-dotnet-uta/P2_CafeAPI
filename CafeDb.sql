492f7ef7b0655bbf75805ac7b7940eaea723d2aa
KyleHillRevature <kyle.hill@revature.net>
2021-09-30 19:03:59 -0400

    updated table scripts

--- CafeDb.sql
+++ CafeDb.sql
@@ -1,18 +1,8 @@
-CREATE DATABASE testproject2;
-
-USE testproject2
-go
-
 --create Customers table
 CREATE TABLE Customer(
 Id INT PRIMARY KEY IDENTITY,
 FirstName varchar(50) NOT NULL,
 LastName varchar(50) NOT NULL);
-INSERT INTO dbo.Customer (FirstName, LastName)
-VALUES ('Rick', 'Torres');
-
-INSERT INTO dbo.Customer (FirstName, LastName)
-VALUES ('Tony', 'Montana');
 
 -- create Administrator table
 CREATE TABLE Administrator(
@@ -24,6 +14,7 @@ EmailAddress varchar(50) NOT NULL,
 
 -- create SingIn table
 CREATE TABLE SignIn(
+Id INT PRIMARY KEY IDENTITY,
 UserName varchar(30) NOT NULL,
 [Password] varchar(30) NOT NULL,
 CustomerId INT FOREIGN KEY REFERENCES Customer(Id) ON DELETE CASCADE,
@@ -31,8 +22,7 @@ AdministratorId INT FOREIGN KEY REFERENCES Administrator(Id) ON DELETE CASCADE)
 
 -- create Products table
 CREATE TABLE Product(
-Id int  PRIMARY KEY IDENTITY,
-ProductId varchar(50) NOT NULL,
+Id varchar(20) PRIMARY KEY ,
 [Name] varchar(50) NOT NULL,
 [Description] varchar(100),
 Price decimal(20,4) NOT NULL,
@@ -47,6 +37,7 @@ TotalPrice decimal(20,4) NOT NULL);
 
 -- create OrderItem table
 create table [OrderItem](
+Id INT PRIMARY KEY IDENTITY,
 OrderId INT NOT NULL FOREIGN KEY REFERENCES [Order](Id) ON DELETE CASCADE,
-ProductId INT NOT NULL FOREIGN KEY REFERENCES Product(Id) ON DELETE CASCADE
+ProductId varchar(20) NOT NULL FOREIGN KEY REFERENCES Product(Id) ON DELETE CASCADE
 )
